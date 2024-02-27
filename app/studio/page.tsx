04838e6e665370770a1af7b3ad721dcf227b158d
NiKHiLkr23 <nikhilkumarofficial23@gmail.com>
2024-02-28 01:47:41 +0530

    fix: image from workspace
    -workaround for now
    bug: error on s3-bucket image url

--- app/studio/page.tsx
+++ app/studio/page.tsx
@@ -8,7 +8,7 @@ function page() {
     <section className=" min-h-screen  w-full pt-[50px]   ">
       <div className="flex items-center justify-center w-full xl:max-w-7xl xl:mx-auto">
         <Image
-          src="https://sei-warriors.s3.amazonaws.com/banner-grey.png"
+          src="/images/banner-grey.png"
           width={1000}
           height={1000}
           alt=""
