d0df797c891ad72cceb0d5aa887a47c63da27355
NiKHiLkr23 <nikhilkumarofficial23@gmail.com>
2024-03-12 22:12:45 +0530

    build: metadata update

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import { siteConfig } from "@/config/site";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
+  metadataBase: new URL("https://seiwarriors.xyz"),
   title: {
     default: siteConfig.name,
     template: `${siteConfig.name} | %s`,
@@ -17,6 +18,34 @@ export const metadata: Metadata = {
       href: "/images/warriors-logo.png",
     },
   ],
+  openGraph: {
+    title: siteConfig.name,
+    description: siteConfig.description,
+    url: "seiwarriors.xyz",
+    siteName: "Sei Warriors",
+    images: [
+      {
+        url: "/images/banner-red.png",
+        width: 1200,
+        height: 630,
+        alt: "Sei Warriors",
+      },
+    ],
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    site: "@SeiWarriorNFT",
+    images: [
+      {
+        url: "/images/banner-red.png",
+        width: 1200,
+        height: 630,
+        alt: "Sei Warriors",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
